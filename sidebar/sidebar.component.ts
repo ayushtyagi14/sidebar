88e715be80798082b175a3dae2cddeb89f0493d2
Ayush Tyagi <ayush.tyagi145@gmail.com>
2022-11-30 21:07:51 +0530

    Add files via upload

---
+++ sidebar/sidebar.component.ts
@@ -0,0 +1,25 @@
+// import { animate, keyframes, style, transition, trigger } from '@angular/animations';
+import { Component, EventEmitter, HostListener, OnInit, Output } from '@angular/core';
+
+
+
+
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css'],
+  
+})
+export class SidebarComponent implements OnInit {
+  
+
+  constructor() { }
+
+  ngOnInit(): void {
+   
+  }
+  
+ 
+
+}
